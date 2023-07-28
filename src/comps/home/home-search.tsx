c39bed2f5f69ac7fc02f556629d0c8a67cc21eef
eddie.tohier <eddie.tohier@nusatrip.com>
2023-07-28 14:03:38 +0700

    update text animate

--- src/comps/home/home-search.tsx
+++ src/comps/home/home-search.tsx
@@ -1,14 +1,12 @@
-import { Container, Group, Tabs, Text } from '@ns-ui/core';
-import { DefaultValue } from '@ns-ui/core/lib/MultiSelect/DefaultValue/DefaultValue';
+import { Group, Tabs, Text } from '@ns-ui/core';
 import { useToggle } from '@ns-ui/hooks';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
 import FaBed from '../icons/FaBed';
 import FaPlane from '../icons/FaPlane';
 import { FlightHome } from '../widget/flight-home';
 import { HotelHome } from '../widget/hotel-home';
 import useStyles from './home-search.styles';
-import AnimatedType from '../home/animated-type';
-import { TextLoop } from 'react-text-loop-next';
 
 const HomeSearch = () => {
     const { classes, cx } = useStyles();
@@ -18,6 +16,17 @@ const HomeSearch = () => {
         'to all your favorite places',
         'at all your favorite places',
     ]);
+
+    const TEXTS = ['in Asia', 'in Europe', 'anywhere in The World'];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(
+            () => setIndex((index) => index + 1),
+            3000 // every 3 seconds
+        );
+        return () => clearTimeout(intervalId);
+    }, []);
 
     // console.info(tabvaluetogle);
     return (
@@ -66,7 +75,10 @@ const HomeSearch = () => {
                         </Tabs.Tab>
                         <Group spacing="8px">
                             {tabvaluetogle}
-                            <TextLoop
+                            <TextTransition springConfig={presets.stiff}>
+                                {TEXTS[index % TEXTS.length]}
+                            </TextTransition>
+                            {/* <TextLoop
                                 // interval={3000}
                                 springConfig={{ stiffness: 180, damping: 16 }}
                                 children={[
@@ -74,8 +86,7 @@ const HomeSearch = () => {
                                     'in Europe.',
                                     'in The World.',
                                 ]}
-                            />
-                            {/* <AnimatedType /> */}
+                            /> */}
                         </Group>
                     </Group>
                 </Tabs.List>
